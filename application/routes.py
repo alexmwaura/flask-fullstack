dfc1f1ec622db63e40aec97ae79cae8db485dd9b
alexmwaura <alexmwaura43@gmail.com>
2022-02-07 12:06:07 +0300

    nav

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,24 @@
 from application import app
 from flask import render_template
 
+
 @app.route("/")
 @app.route("/index")
+@app.route("/home")
+def index():
+    return render_template("index.html", login=True)
 
-def  index():
-    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", login=True)
+
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html", login=True)
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html", login=True)
